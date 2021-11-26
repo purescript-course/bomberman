2415c20795a9689eb95a7c95879af178fd9b0232
eugleo <wybitul.evzen@gmail.com>
2021-11-26 15:21:21 +0100

    Add border wall generation

--- src/Main.purs
+++ src/Main.purs
@@ -1,47 +1,79 @@
-module Main where
+module Main
+  ( Tile
+  , World
+  , draw
+  , height
+  , initial
+  , isBorder
+  , main
+  , movePlayer
+  , reactor
+  , width
+  ) where
 
 import Prelude
 
-import Data.Grid (Coordinates)
+import Data.Grid (Grid, Coordinates)
+import Data.Grid as Grid
 import Data.Maybe (Maybe(..))
+import Debug (spy, traceM)
 import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Console (log)
 import Reactor (Reactor, dimensions, executeDefaultBehavior, getW, runReactor, updateW_)
 import Reactor.Events (Event(..))
 import Reactor.Graphics.Colors as Color
-import Reactor.Graphics.Drawing (Drawing, fill, tile)
-import Reactor.Internal.Helpers (withJust)
+import Reactor.Graphics.Drawing (Drawing, drawGrid, fill, tile)
 import Reactor.Reaction (Reaction)
 
-main :: Effect Unit
-main = runReactor reactor { title: "Boberman", width: 20, height: 20 }
+width :: Int
+width = 11
 
-type World = { player :: Coordinates, grid :: ... }
+height :: Int
+height = 11
+
+main :: Effect Unit
+main = runReactor reactor { title: "Bomberman", width, height }
+
+data Tile = Wall | Empty
+
+derive instance tileEq :: Eq Tile
+
+type World = { player :: Coordinates, board :: Grid Tile }
+
+isBorder :: Coordinates -> Boolean
+isBorder { x, y } = x == 0 || x == (width - 1) || y == 0 || y == (height - 1)
 
 reactor :: Reactor World
 reactor = { initial, draw, handleEvent, isPaused: const true }
 
-wall = ...
-wall2 = ...
-generateWall = ...
-
 initial :: World
-initial = { player: { x: 0, y: 0 } }
+initial = { player: { x: width / 2, y: height / 2 }, board }
+  where
+  board = Grid.construct width height (\point -> if isBorder point then Wall else Empty)
 
 draw :: World -> Drawing
-draw { player } = do
+draw { player, board } = do
+  drawGrid board drawTile
   fill Color.blue400 $ tile player
+  where
+  drawTile Empty = Just Color.green50
+  drawTile Wall = Just Color.gray500
 
 handleEvent :: Event -> Reaction World
 handleEvent event = do
-  { width, height } <- dimensions
-  let
-    clip a m = min (max 0 a) (m - 1)
-    bound { x, y } = { x: clip x width, y: clip y height }
-  { player: { x, y } } <- getW
   case event of
-    KeyPress { key: "ArrowLeft" } -> updateW_ { player: bound { x: x - 1, y } }
-    KeyPress { key: "ArrowRight" } -> updateW_ { player: bound { x: x + 1, y } }
-    KeyPress { key: "ArrowDown" } -> updateW_ { player: bound { x, y: y + 1 } }
-    KeyPress { key: "ArrowUp" } -> updateW_ { player: bound { x, y: y - 1 } }
-
+    KeyPress r@{ key: "ArrowLeft" } -> movePlayer { x: -1, y: 0 }
+    KeyPress { key: "ArrowRight" } -> movePlayer { x: 1, y: 0 }
+    KeyPress { key: "ArrowDown" } -> movePlayer { x: 0, y: 1 }
+    KeyPress { key: "ArrowUp" } -> movePlayer { x: 0, y: -1 }
     _ -> executeDefaultBehavior
+
+movePlayer :: { x :: Int, y :: Int } -> Reaction World
+movePlayer { x: xd, y: yd } = do
+  { player: { x, y }, board } <- getW
+  let newPlayerPosition = { x: x + xd, y: y + yd }
+  when (isEmpty newPlayerPosition board) $
+    updateW_ { player: newPlayerPosition }
+  where
+  isEmpty position board = Grid.index board position == Just Empty
